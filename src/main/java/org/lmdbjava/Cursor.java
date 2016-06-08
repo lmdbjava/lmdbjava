d81a21d7103f60a3496fe4bc64822f6c74dc700b
Kristoffer Sjogren <stoffe@gmail.com>
2016-06-08 18:34:24 +0200

    Cursor javadoc

--- src/main/java/org/lmdbjava/Cursor.java
+++ src/main/java/org/lmdbjava/Cursor.java
@@ -12,6 +12,9 @@ import static org.lmdbjava.ResultCodeMapper.checkRc;
 import static org.lmdbjava.ValueBuffers.createVal;
 import static org.lmdbjava.ValueBuffers.wrap;
 
+/**
+ * A cursor handle.
+ */
 public class Cursor {
 
   private ByteBuffer buffer;
@@ -24,6 +27,14 @@ public class Cursor {
     this.tx = tx;
   }
 
+  /**
+   * <p>
+   *   Close a cursor handle.
+   * </p>
+   *
+   * The cursor handle will be freed and must not be used again after this call.
+   * Its transaction must still be live if it is a write-transaction.
+   */
   public void close() {
     if (!closed) {
       lib.mdb_cursor_close(ptr);
@@ -32,8 +43,8 @@ public class Cursor {
   }
 
   /**
-   * Return count of duplicates for current key.
    * </p>
+   *   Return count of duplicates for current key.
    * <p>
    * This call is only valid on databases that support sorted duplicate data
    * items {@link org.lmdbjava.DatabaseFlags#MDB_DUPSORT}.
@@ -57,7 +68,19 @@ public class Cursor {
   public void delete() throws LmdbNativeException {
     checkRc(lib.mdb_cursor_del(ptr, 0));
   }
-
+  /**
+   * <p>
+   *   Retrieve by cursor.
+   * </p>
+   * This function retrieves key/data pairs from the database. The address and length
+   * of the key are returned in the object to which \b key refers (except for the
+   * case of the #MDB_SET option, in which the \b key object is unchanged), and
+   * the address and length of the data are returned in the object to which \b data
+   *
+   * @param key Placeholder for the key memory address to be wrapped.
+   * @param val Placeholder for the value memory address to be wrapped.
+   * @param op A cursor operation.
+   */
   public void get(ByteBuffer key, ByteBuffer val, CursorOp op)
       throws LmdbNativeException {
     requireNonNull(key);
@@ -82,6 +105,16 @@ public class Cursor {
     wrap(val, v);
   }
 
+  /**
+   * <p>
+   *   Store by cursor.
+   * </p>
+   *
+   * @param key The key operated on.
+   * @param val The data operated on.
+   *
+   * @param op Options for this operation.
+   */
   public void put(ByteBuffer key, ByteBuffer val, PutFlags... op)
       throws LmdbNativeException {
     requireNonNull(key);
@@ -98,6 +131,20 @@ public class Cursor {
     checkRc(lib.mdb_cursor_put(ptr, k, v, flags));
   }
 
+  /**
+   * <p>
+   *   Renew a cursor handle.
+   * </p>
+   *
+   * A cursor is associated with a specific transaction and database.
+   * Cursors that are only used in read-only
+   * transactions may be re-used, to avoid unnecessary malloc/free overhead.
+   * The cursor may be associated with a new read-only transaction, and
+   * referencing the same database handle as it was created with.
+   * This may be done whether the previous transaction is live or dead.
+   *
+   * @param tx transaction handle
+   */
   public void renew(Transaction tx) throws LmdbNativeException {
     if (!tx.isReadOnly()) {
       throw new IllegalArgumentException("cannot renew write transactions");
