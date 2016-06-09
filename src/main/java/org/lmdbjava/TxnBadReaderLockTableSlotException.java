bfa09f59b2fba14677a33f08135ba9b4a1932b8b
Ben Alex <ben.alex@acegi.com.au>
2016-06-09 11:30:10 +1000

    Add LICENSE.txt and make owner identifiable

--- src/main/java/org/lmdbjava/TxnBadReaderLockTableSlotException.java
+++ src/main/java/org/lmdbjava/TxnBadReaderLockTableSlotException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 LmdbJava
+ * Copyright 2016 The LmdbJava Project, http://lmdbjava.org/
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
