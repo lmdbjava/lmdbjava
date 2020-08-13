bc0ccbc64b4025550462c64e3fa5b0fab5735ce4
Jiri <jvanek@redhat.com>
2020-08-13 21:03:29 +0200

    Added usage of aarch64 build of native llmdb
     * Added more distinguished names for x86_64, aarch64, ppc64le and  unified arch64  for all three linuxes
     * Renamed unifying x86_64 varable to make spotbugs happy

--- src/main/java/org/lmdbjava/Library.java
+++ src/main/java/org/lmdbjava/Library.java
@@ -102,8 +102,12 @@ final class Library {
     final String libToLoad;
 
     final String arch = getProperty("os.arch");
-    final boolean arch64 = "x64".equals(arch) || "amd64".equals(arch)
+    final boolean x64 = "x64".equals(arch) || "amd64".equals(arch)
                                || "x86_64".equals(arch);
+    final boolean aarch64 = "aarch64".equals(arch);
+    final boolean ppc64le = "ppc64le".equals(arch);
+
+    // final boolean arch64 = x64 || aarch64 || ppc64le;
 
     final String os = getProperty("os.name");
     final boolean linux = os.toLowerCase(ENGLISH).startsWith("linux");
@@ -112,11 +116,15 @@ final class Library {
 
     if (SHOULD_USE_LIB) {
       libToLoad = getProperty(LMDB_NATIVE_LIB_PROP);
-    } else if (SHOULD_EXTRACT && arch64 && linux) {
+    } else if (SHOULD_EXTRACT && aarch64 && linux) {
+      libToLoad = extract("org/lmdbjava/lmdbjava-native-linux-aarch64.so");
+    } else if (SHOULD_EXTRACT && ppc64le && linux) {
+      libToLoad = extract("org/lmdbjava/lmdbjava-native-linux-ppc64le.so");
+    } else if (SHOULD_EXTRACT && x64 && linux) {
       libToLoad = extract("org/lmdbjava/lmdbjava-native-linux-x86_64.so");
-    } else if (SHOULD_EXTRACT && arch64 && osx) {
+    } else if (SHOULD_EXTRACT && x64 && osx) {
       libToLoad = extract("org/lmdbjava/lmdbjava-native-osx-x86_64.dylib");
-    } else if (SHOULD_EXTRACT && arch64 && windows) {
+    } else if (SHOULD_EXTRACT && x64 && windows) {
       libToLoad = extract("org/lmdbjava/lmdbjava-native-windows-x86_64.dll");
     } else {
       libToLoad = LIB_NAME;
